1196b1dad6de1b77b8143434859f08a763112426
Roman Chernega <roman-chernega@yandex.ru>
2021-12-22 19:06:32 +0300

    A_DS_les6_Trees_update

--- src/main/java/HW_06/Test.java
+++ src/main/java/HW_06/Test.java
@@ -1,15 +1,22 @@
 package HW_06;
 
-import java.util.Random;
 
 public class Test {
 
     public static void main(String[] args) {
 
+        int countTree = 0;
+
+        while (countTree < 20) {
+            RB_Tree();
+            countTree++;
+        }
+    }
+
+    private static void RB_Tree() {
         Tree<Integer> tree = new TreeImpl<>();
         int min = -25;
         int max = 25;
-
         tree.add((int) (Math.random() * ((max-min)+1)+min));
         tree.add((int) (Math.random() * ((max-min)+1)+min));
         tree.add((int) (Math.random() * ((max-min)+1)+min));
@@ -17,23 +24,8 @@ public class Test {
         tree.add((int) (Math.random() * ((max-min)+1)+min));
         tree.add((int) (Math.random() * ((max-min)+1)+min));
         tree.add((int) (Math.random() * ((max-min)+1)+min));
-        tree.add((int) (Math.random() * ((max-min)+1)+min));
-        tree.add((int) (Math.random() * ((max-min)+1)+min));
-        tree.add((int) (Math.random() * ((max-min)+1)+min));
-        tree.add((int) (Math.random() * ((max-min)+1)+min));
-
-
         tree.display();
         System.out.println("Размер дерева равен " + "'" +  tree.size() + "'");
-
-//        tree.remove(31);
-//        tree.remove(42);
-//        tree.remove(70);
-//        tree.display();
-
-//        tree.traverse(CW_06.Tree.TraversMode.IN_ORDER);
-
-
-
     }
+
 }
